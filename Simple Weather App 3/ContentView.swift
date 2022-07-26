e0b856b6669715085e1cc7fbe0950ed6fef91002
KimonoBoi <danloui3@hotmail.com>
2022-07-25 20:28:57 -0500

    Initial Commit

---
+++ Simple Weather App 3/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Simple Weather App 3
+//
+//  Created by Daniel L Gliniak on 7/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple Weather App 3/Simple_Weather_App_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Weather_App_3App.swift
+//  Simple Weather App 3
+//
+//  Created by Daniel L Gliniak on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Weather_App_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
